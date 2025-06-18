5eb7c0a2ce1028c2de7fc25217bfbb58ed013712
Asad Ali <asadjattofficials@gmail.com>
2025-06-19 00:00:00 +0500

    attributes in js

---
+++ attributes in js/script.js
@@ -0,0 +1,11 @@
+// console.log(document.querySelector(".container").hasAttribute("style"));
+
+
+// console.log(document.querySelector(".container").getAttribute("style"));
+
+// document.querySelector(".container").setAttribute("id", "myBox");
+
+// console.log(document.querySelector(".container").attributes)
+
+// document.querySelector(".container").removeAttribute("style")
+
