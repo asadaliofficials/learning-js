64fad034f79f3636c7c8455a244acc4ca7e6a0d2
Asad Ali <asadjattofficials@gmail.com>
2025-06-28 00:00:00 +0500

    element append in js

---
+++ element append in js/script.js
@@ -0,0 +1,50 @@
+// inserting elements 
+// let h1 = document.createElement('h1');
+// h1.innerHTML = 'hello world';
+// document.querySelector('.container').prepend(h1);
+
+
+// document.querySelector('.container').append(h1);
+
+
+// document.querySelector('.container').before(h1);
+
+
+// document.querySelector('.container').after(h1);
+
+// document.querySelector('.container').replaceWith(h1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// inserting html/text/element 
+// html 
+// document.querySelector('.container').insertAdjacentHTML("beforeend", '<h1>hello world 2</h1>');
+// document.querySelector('.container').insertAdjacentHTML("afterend", '<h1>hello world 3</h1>');
+// document.querySelector('.container').insertAdjacentHTML("afterbegin", '<h1>hello world 4</h1>');
+// document.querySelector('.container').insertAdjacentHTML("beforebegin", '<h1>hello world 5</h1>');
+
+
+// text 
+// document.querySelector('#para').insertAdjacentText("afterbegin", 'hello i am text inserted after ');
+// document.querySelector('#para').insertAdjacentText("beforebegin", 'hello i am text inserted before');
+// document.querySelector('#para').insertAdjacentText("afterend", 'hello i am text inserted after end ');
+// document.querySelector('#para').insertAdjacentText("beforeend", 'hello i am text inserted before end');
+
+
+// element 
+
+// let h1 = document.createElement('h1');
+// h1.innerHTML = 'hello world';
+// document.querySelector('#para').insertAdjacentElement("afterbegin", h1);
