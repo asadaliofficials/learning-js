46d0f3e27303875a3c5d450152fb678daf5038db
Asad Ali <asadjattofficials@gmail.com>
2025-07-13 00:00:00 +0500

    Math Object

---
+++ Math Object/index.js
@@ -0,0 +1,34 @@
+// math absolute
+// tells difference from zero
+console.log(Math.abs(-4.4))
+  
+ 
+// math round
+// return nearest complete integer
+console.log(Math.round(4.4))
+
+// math floor
+// return nearest integer below and up if value is in minus
+console.log(Math.floor(4.9))
+console.log(Math.floor(-4.9))
+
+// math ceil 
+// return the value to up
+console.log(Math.ceil(4.1))
+
+
+// math pow 
+// returns the value of x to the power of y 
+console.log(Math.pow(3, 3))
+console.log(3 ** 3)
+
+
+// math square root 
+console.log(Math.sqrt(36))
+
+
+// math random 
+
+let random = (Math.round(Math.random() * 100))
+
+console.log(random)
