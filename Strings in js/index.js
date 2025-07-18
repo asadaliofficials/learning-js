baa9615f4a74357091edd76bf2d4a9959192d49b
Asad Ali <asadjattofficials@gmail.com>
2025-07-19 00:00:00 +0500

    Strings in js

---
+++ Strings in js/index.js
@@ -0,0 +1,28 @@
+let myName = "Asad Ali";
+
+console.log(myName[0]);
+console.log(myName[1]);
+console.log(myName[2]);
+console.log(myName[3]);
+console.log(myName[4]);
+console.log(myName[5]);
+console.log(myName[6]);
+console.log(myName[7]);
+
+console.log(myName.length)
+
+
+let friendsName = "Lucky"
+
+console.log(`My name is ${myName} and my friends name is ${friendsName}`)
+
+
+
+let companyName = "Lucky"
+console.log(companyName.slice(1,4))
+console.log(companyName.replace("uc", 55))
+
+console.log(companyName.concat(friendsName))
+
+
+console.log(myName.trim())
