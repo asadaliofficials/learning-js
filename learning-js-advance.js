0763d656b358fb8b924aeed2adf81fe3011379a1
Asad Ali <asadjattofficials@gmail.com>
2025-07-25 00:00:00 +0500

    Learning: Array Reduce Example

--- learning-js-advance.js
+++ learning-js-advance.js
@@ -56,3 +56,9 @@ let a = null;
 let b = a ?? "default";
 console.log(b);
 
+
+// Array Reduce Example
+const nums = [1, 2, 3, 4];
+const sum = nums.reduce((acc, n) => acc + n, 0);
+console.log(sum);
+
