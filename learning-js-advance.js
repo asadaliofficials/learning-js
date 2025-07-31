8f66b28599dcd6d66d1f00779e91db001c1f6ca1
Asad Ali <asadjattofficials@gmail.com>
2025-08-01 00:00:00 +0500

    Learning: Modules Example

--- learning-js-advance.js
+++ learning-js-advance.js
@@ -84,3 +84,14 @@ setTimeout(() => console.log("Timeout"), 0);
 Promise.resolve().then(() => console.log("Promise"));
 console.log("End");
 
+
+// Modules Example
+// file: math.js
+export function add(a, b) {
+  return a + b;
+}
+
+// file: index.js
+import { add } from "./math.js";
+console.log(add(2, 3));
+
