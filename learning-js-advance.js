9a1740276b1f3281661196f8ecb84ba4b5b9496f
Asad Ali <asadjattofficials@gmail.com>
2025-08-02 00:00:00 +0500

    Learning: Spread Operator Example

--- learning-js-advance.js
+++ learning-js-advance.js
@@ -298,3 +298,9 @@ const arr = [1, 2, 3];
 const arr2 = [...arr, 4, 5];
 console.log(arr2);
 
+
+// Spread Operator Example
+const arr = [1, 2, 3];
+const arr2 = [...arr, 4, 5];
+console.log(arr2);
+
