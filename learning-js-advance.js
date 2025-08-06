532852655783474d362eecd400a0d846f8adf538
Asad Ali <asadjattofficials@gmail.com>
2025-08-07 00:00:00 +0500

    Learning: Currying Example

--- learning-js-advance.js
+++ learning-js-advance.js
@@ -152,3 +152,12 @@ fetch("https://jsonplaceholder.typicode.com/posts/1")
   .then(res => res.json())
   .then(data => console.log(data));
 
+
+// Currying Example
+function add(a) {
+  return function(b) {
+    return a + b;
+  };
+}
+console.log(add(5)(3));
+
