c0530b18c57d108915c0a493337898c6e0df9ee3
Asad Ali <asadjattofficials@gmail.com>
2025-08-16 00:00:00 +0500

    Learning: While loop vs Do While loop

--- learning-js-advance.js
+++ learning-js-advance.js
@@ -661,3 +661,16 @@ const person = { name: "Ali", age: 25 };
 const { name, age } = person;
 console.log(name, age);
 
+
+// While loop vs Do While loop
+let i = 0;
+while (i < 3) {
+  console.log("While:", i);
+  i++;
+}
+let j = 0;
+do {
+  console.log("Do While:", j);
+  j++;
+} while (j < 3);
+
