9009a0ee0875bffb8b74ad02faee775267ce751c
Asad Ali <asadjattofficials@gmail.com>
2025-08-17 00:00:00 +0500

    Learning: Event Loop Example

--- learning-js-advance.js
+++ learning-js-advance.js
@@ -697,3 +697,10 @@ setTimeout(() => console.log("Timeout"), 0);
 Promise.resolve().then(() => console.log("Promise"));
 console.log("End");
 
+
+// Event Loop Example
+console.log("Start");
+setTimeout(() => console.log("Timeout"), 0);
+Promise.resolve().then(() => console.log("Promise"));
+console.log("End");
+
