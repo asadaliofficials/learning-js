d25d5e6ef5a930632c6afe0ae0c845583c8aee85
Asad Ali <asadjattofficials@gmail.com>
2025-08-23 00:00:00 +0500

    Learning: While loop vs Do While loop

--- learning-js-advance.js
+++ learning-js-advance.js
@@ -350,3 +350,17 @@ const log = debounce(() => console.log("Debounced!"), 1000);
 log();
 log();
 
+
+// While loop vs Do While loop
+let i = 0;
+while (i < 3) {
+  console.log("While:", i);
+  i++;
+}
+
+let j = 0;
+do {
+  console.log("Do While:", j);
+  j++;
+} while (j < 3);
+
