23202f0dda3506a250b5e05922a52a4dd4b3138d
Asad Ali <asadjattofficials@gmail.com>
2025-08-29 00:00:00 +0500

    Learning: Optional Chaining Example

--- learning-js-advance.js
+++ learning-js-advance.js
@@ -1113,3 +1113,9 @@ export function add(a, b) { return a + b; }
 import { add } from "./math.js";
 console.log(add(2, 3));
 
+
+// Optional Chaining Example
+const user = { profile: { email: "test@gmail.com" } };
+console.log(user?.profile?.email);
+console.log(user?.account?.password);
+
