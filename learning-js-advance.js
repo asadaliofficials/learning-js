b64836a0926da086934005935a32c1094626541d
Asad Ali <asadjattofficials@gmail.com>
2025-08-31 00:00:00 +0500

    Learning: While loop vs Do While loop

--- learning-js-advance.js
+++ learning-js-advance.js
@@ -1176,3 +1176,16 @@ class Dog extends Animal {
 }
 new Dog("Rex").speak();
 
+
+// While loop vs Do While loop
+let i = 0;
+while (i < 3) {
+  console.log("While:", i);
+  i++;
+}
+let j = 0;
+do {
+  console.log("Do While:", j);
+  j++;
+} while (j < 3);
+
