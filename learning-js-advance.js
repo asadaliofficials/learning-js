a8e7fa7655c9100bf5b5f0d2a2b90d35df1079c5
Asad Ali <asadjattofficials@gmail.com>
2025-09-04 00:00:00 +0500

    Learning: Async Await Example

--- learning-js-advance.js
+++ learning-js-advance.js
@@ -462,3 +462,12 @@ async function getPost() {
 }
 getPost();
 
+
+// Async Await Example
+async function getPost() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+  const data = await res.json();
+  console.log(data);
+}
+getPost();
+
