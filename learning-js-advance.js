ed0c22c9e455f1764352911246b8aff591766721
Asad Ali <asadjattofficials@gmail.com>
2025-09-06 00:00:00 +0500

    Learning: Array Reduce Example

--- learning-js-advance.js
+++ learning-js-advance.js
@@ -1321,3 +1321,9 @@ Person.prototype.sayHi = function() {
 const p = new Person("Ali");
 p.sayHi();
 
+
+// Array Reduce Example
+const nums = [1, 2, 3, 4];
+const sum = nums.reduce((acc, n) => acc + n, 0);
+console.log(sum);
+
