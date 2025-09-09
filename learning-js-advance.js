15774ef83bb5edbe1491bdd219dd50baa12a4540
Asad Ali <asadjattofficials@gmail.com>
2025-09-10 00:00:00 +0500

    Learning: Optional Chaining Example

--- learning-js-advance.js
+++ learning-js-advance.js
@@ -1433,3 +1433,9 @@ const person = { name: "Ali", age: 25 };
 const { name, age } = person;
 console.log(name, age);
 
+
+// Optional Chaining Example
+const user = { profile: { email: "test@gmail.com" } };
+console.log(user?.profile?.email);
+console.log(user?.account?.password);
+
