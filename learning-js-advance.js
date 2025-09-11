d21c86ebbef7f55fc7173b8c61b443c9f25b9fb9
Asad Ali <asadjattofficials@gmail.com>
2025-09-12 00:00:00 +0500

    Learning: Fetch API Example

--- learning-js-advance.js
+++ learning-js-advance.js
@@ -1500,3 +1500,9 @@ Person.prototype.sayHi = function() {
 const p = new Person("Ali");
 p.sayHi();
 
+
+// Fetch API Example
+fetch("https://jsonplaceholder.typicode.com/posts/1")
+  .then(res => res.json())
+  .then(data => console.log(data));
+
