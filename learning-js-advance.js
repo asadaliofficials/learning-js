66080ef6eb861beed0d81e41dff9e672936a4c7a
Asad Ali <asadjattofficials@gmail.com>
2025-09-12 00:00:00 +0500

    Learning: Map vs WeakMap

--- learning-js-advance.js
+++ learning-js-advance.js
@@ -1515,3 +1515,12 @@ async function getPost() {
 }
 getPost();
 
+
+// Map vs WeakMap
+let map = new Map();
+map.set("a", 1);
+console.log(map.get("a"));
+let wm = new WeakMap();
+let obj = {};
+wm.set(obj, "value");
+
