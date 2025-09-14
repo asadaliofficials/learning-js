eda0b4c6c20f5a27145fd2a8461b7038d2b1479e
Asad Ali <asadjattofficials@gmail.com>
2025-09-15 00:00:00 +0500

    Learning: Closures Example

--- learning-js-advance.js
+++ learning-js-advance.js
@@ -1577,3 +1577,15 @@ async function getPost() {
 }
 getPost();
 
+
+// Closures Example
+function outer() {
+  let count = 0;
+  return function inner() {
+    count++;
+    return count;
+  };
+}
+const counter = outer();
+console.log(counter());
+
