d28a2412cff977687ea36c4e35e0a5335589a50e
Asad Ali <asadjattofficials@gmail.com>
2025-09-20 00:00:00 +0500

    Learning: Currying Example

--- learning-js-advance.js
+++ learning-js-advance.js
@@ -1756,3 +1756,12 @@ function throttle(fn, limit) {
 const log = throttle(() => console.log("Throttled!"), 1000);
 log(); log();
 
+
+// Currying Example
+function add(a) {
+  return function(b) {
+    return a + b;
+  };
+}
+console.log(add(5)(3));
+
