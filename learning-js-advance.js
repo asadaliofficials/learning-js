2bed3289f0368777066d64cf44603a22febb9849
Asad Ali <asadjattofficials@gmail.com>
2025-09-23 00:00:00 +0500

    Learning: Optional Chaining Example

--- learning-js-advance.js
+++ learning-js-advance.js
@@ -1897,3 +1897,9 @@ let wm = new WeakMap();
 let obj = {};
 wm.set(obj, "value");
 
+
+// Optional Chaining Example
+const user = { profile: { email: "test@gmail.com" } };
+console.log(user?.profile?.email);
+console.log(user?.account?.password);
+
