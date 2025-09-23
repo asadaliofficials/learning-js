e4598db69a802f9d038fdc97cdbdd992175508bc
Asad Ali <asadjattofficials@gmail.com>
2025-09-24 00:00:00 +0500

    Learning: Event Loop Example

--- learning-js-advance.js
+++ learning-js-advance.js
@@ -1921,3 +1921,10 @@ const arr = [1, 2, 3];
 const arr2 = [...arr, 4, 5];
 console.log(arr2);
 
+
+// Event Loop Example
+console.log("Start");
+setTimeout(() => console.log("Timeout"), 0);
+Promise.resolve().then(() => console.log("Promise"));
+console.log("End");
+
